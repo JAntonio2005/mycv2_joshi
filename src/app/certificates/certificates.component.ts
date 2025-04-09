0f517c41c0a23daef3a1cde7ff5300c9462e2f7b
JAntonio2005 <joseantinioortizhernandez777@gmail.com>
2025-04-08 20:49:43 -0600

    Subí todo hasta services

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CertificatesService } from '../services/certificates-service/certificates.service';
 
 @Component({
   selector: 'app-certificates',
   templateUrl: './certificates.component.html',
-  styleUrl: './certificates.component.css'
+  styleUrls: ['./certificates.component.css']
 })
 export class CertificatesComponent {
 
+  constructor(public certificatesService: CertificatesService) {
+    console.log(this.certificatesService);
+  }
 }
